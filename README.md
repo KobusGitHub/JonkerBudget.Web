a333179754bd8bb75f522e613769a3edccb93db6
Kobus Jonker <kobus.jonker@sgits.co.za>
2017-05-25 21:52:19 +0200

    Website

--- README.md
+++
@@ -1,2 +0,0 @@
-# JonkerBudget.Web
-Test
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component, ViewContainerRef} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<router-outlet></router-outlet>'
+})
+export class AppComponent {
+  public title = 'app works!';
+
+  public constructor(private viewContainerRef: ViewContainerRef) {
+    
+    
+  }
+
+
+}

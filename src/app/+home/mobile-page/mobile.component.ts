78c8320300f7937898b145c56e072a3bd5a6f81a
Kobus Jonker <kobus.jonker@sgits.co.za>
2017-06-08 19:28:20 +0200

    Make offline and pro apk downloadable

--- src/app/+home/mobile-page/mobile.component.ts
+++ src/app/+home/mobile-page/mobile.component.ts
@@ -13,6 +13,7 @@ export class MobileComponent implements OnInit {
   showPassword: boolean = false;
   password: string = '';
   wantAndroid = false;
+  wantAndroidPro = false;
 
   ngOnInit() {
    
@@ -20,27 +21,53 @@ export class MobileComponent implements OnInit {
 
   android(){
     this.wantAndroid = true;
+    this.wantAndroidPro = false;
+    this.showImage = false;
+    this.showPassword = true;
+  }
+
+  androidPro() {
+    this.wantAndroid = false;
+    this.wantAndroidPro = true;
     this.showImage = false;
     this.showPassword = true;
   }
 
   download(){
-    this.wantAndroid = false;
-    this.showImage = true;
-    this.showPassword = false;
-
-    if(this.password === 'Walle'){
-      this.password = '';
-      window.open(
-        'http://www.crazyjonker.co.za/mobileapk/jonker-budget.apk',
-        '_blank' // <- This is what makes it open in a new window.
-      );
     
-    } else {
-      this.password = '';
-      alert("Incorrect password")
+
+    if(this.wantAndroid){
+      if(this.password === 'crazyguest'){
+        this.password = '';
+        window.open(
+          'http://www.crazyjonker.co.za/mobileapk/home-budget-offline.apk',
+          '_blank' // <- This is what makes it open in a new window.
+        );
+      
+      } else {
+        this.password = '';
+        alert("Incorrect password")
+      }
     }
 
+    if (this.wantAndroidPro) {
+      if (this.password === 'crazywalle') {
+        this.password = '';
+        window.open(
+          'http://www.crazyjonker.co.za/mobileapk/home-budget-online.apk',
+          '_blank' // <- This is what makes it open in a new window.
+        );
+
+      } else {
+        this.password = '';
+        alert("Incorrect password")
+      }
+    }
+
+    this.wantAndroid = false;
+    this.wantAndroidPro = false;
+    this.showImage = true;
+    this.showPassword = false;
   }
 
 }
